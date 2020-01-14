403443d6d7dc0b4ffa32a5a9275416c4ebacdaed
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 17:27:30 +0200

    Add project files.

---
+++ 47.uzd Method PrintGreeting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _47.uzd_Method_PrintGreeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                PrintGreeting();
+            }
+        }
+
+        private static void PrintGreeting()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+// Definēt metodi PrintGreeting, kura console izdrukā tekstu “Hello Wordl!”. Izdrukāt šo sveicienu 3 reizes.
